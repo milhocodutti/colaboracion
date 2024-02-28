4a93dc8ccd9a8e30b0cff1437ac7053153227ea5
milhocodutti <emicodutti@gmail.com>
2024-02-27 23:29:22 -0300

    Guia funciones
    Archivo repositorio de definiciones, funciones, metodos y programas basicos.

---
+++ funciones.py
@@ -0,0 +1,12 @@
+def suma(num1, num2):
+    suma=num1+num2
+    return suma
+
+if __name__=="__main__":
+    print("Hola, esta esta es un apicacion para sumar:")
+    print("Ingrese primer valor:")
+    num1=float(input())
+    print("Ingrese segundo valor:")
+    num2=float(input())
+    resultado=num1+num2
+    print("El resultado es:",resultado)
